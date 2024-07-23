46813108b5c42963fde06349a861165324f68190
Loreti <pedroloreti639@gmail.com>
2024-07-22 22:43:22 -0300

    Tarefa 3 concluída

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import { applyInputRangeStyle } from "./inputRange.js";
+import { albumList } from "./albumsDatabase.js";
+
 function genresLogic() {
     const buttons = document.querySelectorAll('.button__gender');
     buttons.forEach(button => {
@@ -10,3 +13,85 @@ function genresLogic() {
 }
 
 genresLogic()
+
+function routine() {
+    applyInputRangeStyle()
+}
+
+routine()
+
+
+function createAlbumCard(album) {
+    // Criação do elemento de lista
+    const card = document.createElement('li');
+    card.className = 'list__itens';
+    console.log(card)
+
+    // Criação e configuração da imagem
+    const img = document.createElement('img');
+    img.src = album.img;
+    img.alt = album.title
+    card.appendChild(img)
+
+    // Criação e configuração do título
+
+    const title = document.createElement('h3');
+    title.innerText = album.title
+    title.className = 'title--h3';
+    card.appendChild(title)
+
+    // Criação do contêiner de descrição
+
+    const descriptionContainer = document.createElement('div');
+    descriptionContainer.className = 'album__container--descricao'
+
+    // Criação e adição do parágrafo com o nome da banda
+    const band = document.createElement('p');
+    band.innerText = album.band
+    descriptionContainer.appendChild(band);
+
+    // Criação e adição do parágrafo com o gênero
+    const genre = document.createElement('p')
+    genre.innerText = album.genre
+    descriptionContainer.appendChild(genre);
+
+    // Adiciona o contêiner de descrição ao card
+    card.appendChild(descriptionContainer);
+
+    // Criação do contêiner de preço e botão
+
+    const priceButtonContainer = document.createElement('div');
+    priceButtonContainer.className = 'price__button--container';
+
+    // Criação e adição do parágrafo com o preço
+
+    const price = document.createElement('p');
+    price.className = 'price';
+    price.innerText = `R$ ${album.price}`;
+    priceButtonContainer.appendChild(price);
+
+    // Criação e adição do botão
+
+    const button = document.createElement('button');
+    button.innerText = 'Comprar';
+    button.className = 'button__buy'
+    priceButtonContainer.appendChild(button);
+    // Adiciona o contêiner de preço e botão ao card
+
+    card.appendChild(priceButtonContainer);
+
+    return card
+}
+
+function renderAlbumCards(albums) {
+    const container = document.querySelector('ul')
+
+    container.innerHTML = '';
+    albums.forEach(album => {
+        const albumCard = createAlbumCard(album);
+        
+        container.appendChild(albumCard);
+    });
+}
+
+renderAlbumCards(albumList)
