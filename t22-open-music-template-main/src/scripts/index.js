2ce2b224d821a85eee25341fba3a2c98b4f905bd
Loreti <111466632+PedroLoreti@users.noreply.github.com>
2024-07-31 11:36:49 -0300

    Tarefa 4 concluída

---
+++ t22-open-music-template-main/src/scripts/index.js
@@ -0,0 +1,124 @@
+import { applyInputRangeStyle } from "./inputRange.js";
+import { albumList } from "./albumsDatabase.js";
+import { darkMode } from "./theme.js";
+
+function genresLogic() {
+    const buttons = document.querySelectorAll('.button__gender');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            buttons.forEach(btn => btn.classList.remove('black'));
+
+            button.classList.add('black')
+        })
+    })
+}
+
+genresLogic()
+
+function routine() {
+    applyInputRangeStyle()
+    darkMode()
+    leitorInput()
+}
+
+routine()
+
+
+function createAlbumCard(album) {
+    // Criação do elemento de lista
+    const card = document.createElement('li');
+    card.className = 'list__itens';
+
+    // Criação e configuração da imagem
+    const img = document.createElement('img');
+    img.src = album.img;
+    img.alt = album.title
+    card.appendChild(img)
+
+    // Criação e configuração do título
+
+    const title = document.createElement('h3');
+    title.innerText = album.title
+    title.className = 'title--h3';
+    card.appendChild(title)
+
+    // Criação do contêiner de descrição
+
+    const descriptionContainer = document.createElement('div');
+    descriptionContainer.className = 'album__container--descricao'
+
+    // Criação e adição do parágrafo com o nome da banda
+    const band = document.createElement('p');
+    band.innerText = album.band
+    descriptionContainer.appendChild(band);
+
+    // Criação e adição do parágrafo com o gênero
+    const genre = document.createElement('p')
+    genre.innerText = album.genre
+    descriptionContainer.appendChild(genre);
+
+    // Adiciona o contêiner de descrição ao card
+    card.appendChild(descriptionContainer);
+
+    // Criação do contêiner de preço e botão
+
+    const priceButtonContainer = document.createElement('div');
+    priceButtonContainer.className = 'price__button--container';
+
+    // Criação e adição do parágrafo com o preço
+
+    const price = document.createElement('p');
+    price.className = 'price';
+    price.innerText = `R$ ${album.price}`;
+    priceButtonContainer.appendChild(price);
+
+    // Criação e adição do botão
+
+    const button = document.createElement('button');
+    button.innerText = 'Comprar';
+    button.className = 'button__buy'
+    priceButtonContainer.appendChild(button);
+    // Adiciona o contêiner de preço e botão ao card
+
+    card.appendChild(priceButtonContainer);
+
+    return card
+}
+
+function renderAlbumCards(albums) {
+    const container = document.querySelector('ul');
+    const input = document.querySelector('#input__range');
+
+    function updateInput() {
+        const inputValue = parseFloat(input.value);
+
+        const filteredAlbums = albums.filter(album => album.price <= inputValue);
+
+        container.innerHTML = '';
+        filteredAlbums.forEach(album => {
+            const albumCard = createAlbumCard(album);
+            container.appendChild(albumCard);
+        });
+    }
+
+    input.addEventListener('input', updateInput);
+    updateInput();
+ 
+}
+
+renderAlbumCards(albumList)
+
+
+function leitorInput() {
+    const input = document.querySelector('#input__range');
+    const rangePrice = document.querySelector('.range__price')
+    
+    function updateRangeValue () {
+        const inputValue = input.value
+        rangePrice.textContent = `R$ ${parseFloat(inputValue).toFixed(2)}`;
+    }
+
+    input.addEventListener('input', updateRangeValue);
+
+    updateRangeValue();
+}
---
+++ t22-open-music-template-main/src/scripts/inputRange.js
@@ -0,0 +1,9 @@
+export function applyInputRangeStyle() {
+    const inputRange = document.querySelector("#input__range");
+    inputRange.addEventListener("input", (event) => {
+      const currentInputValue = event.target.value;
+      const runnableTrackProgress = (currentInputValue / inputRange.max) * 100;
+  
+      inputRange.style.background = `linear-gradient(to right, var(--color-brand-1) ${runnableTrackProgress}%, var( --color-gray-5) ${runnableTrackProgress}%)`;
+    });
+  }
