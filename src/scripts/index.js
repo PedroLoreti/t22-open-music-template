8577f67c22380a448e4d1add47665f97b4875abb
Loreti <pedroloreti639@gmail.com>
2024-08-01 18:46:39 -0300

    update importscript

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,6 @@
-import { applyInputRangeStyle } from ".inputRange.js";
-// import { albumList } from ".albumsDatabase.js";
-import { darkMode } from ".theme.js";
-import { fetchMusics } from ".api.js";
+import { applyInputRangeStyle } from "./inputRange.js";
+import { darkMode } from "./theme.js";
+import { fetchMusics } from "./api.js";
 
 
 
